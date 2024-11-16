cf66f46e5e01810b550621177e65605c1a5ca80a
ChristianW <cwittern@yahoo.com>
2024-11-16 20:22:20 +0900

    Quartz sync: Nov 16, 2024, 8:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: null,
     locale: "en-US",
     baseUrl: "docs.hxwd.org",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "Templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
