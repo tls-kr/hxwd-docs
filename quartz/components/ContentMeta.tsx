9d0b9107901416f68d2f4949b51e998b9b3c63c2
ChristianW <cwittern@yahoo.com>
2025-05-19 15:36:31 +0900

    Quartz sync: May 19, 2025, 3:36 PM

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -1,4 +1,4 @@
-import { Date, getDate } from "./Date"
+import { Date, getDate, formatDate } from "./Date"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import readingTime from "reading-time"
 import { classNames } from "../util/lang"
